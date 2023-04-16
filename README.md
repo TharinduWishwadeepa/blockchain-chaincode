f88d2f7822be8a0bc1ed6e62f1b2e0af724e78e9
TharinduWishwadeepa <dmtwishwad@gmail.com>
2023-04-16 12:50:36 +0530

    added 3rd party functions

---
+++ README.md
@@ -0,0 +1,3 @@
+# blockchainid-javascript
+
+Chaincode
---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const citizenTransactions = require('./lib/citizenTransactions');
+
+module.exports.citizenTransactions = citizenTransactions;
+module.exports.contracts = [citizenTransactions];
